205012e411b000f576acfa690cadd40e6c2a3713
ren831 <rene83120@gmail.com>
2023-05-01 14:54:33 -0700

    updated

---
+++ slot-game/main.py
@@ -0,0 +1,9 @@
+def deposit():
+    while True:
+        amount = input("How much would you like to deposit? $ ")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Please enter a positive number.")
